3e7e2268c03f083aec6e3d541ba57674e85a59a9
Lee <253660689@qq.com>
2020-08-11 10:54:27 +0800

    audioUnitdemo初始化

---
+++ audioUnitDemo/audioUnitDemo/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  audioUnitDemo
+//
+//  Created by Lee Li on 2020/8/11.
+//  Copyright © 2020 Lee Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
