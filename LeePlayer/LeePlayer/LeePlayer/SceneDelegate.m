a777bd62943c65b6f4765de6985c9e1c983a6a14
Lee <253660689@qq.com>
2020-08-19 17:35:50 +0800

    [LeePlayer]ios13用的是sceneDelegate.m文件,suspend打开pip

--- LeePlayer/LeePlayer/LeePlayer/SceneDelegate.m
+++ LeePlayer/LeePlayer/LeePlayer/SceneDelegate.m
@@ -41,7 +41,7 @@
 - (void)sceneWillResignActive:(UIScene *)scene {
     // Called when the scene will move from an active state to an inactive state.
     // This may occur due to temporary interruptions (ex. an incoming phone call).
-//    [SharedPlayerManager pipControllerSuspend];
+    [SharedPlayerManager pipControllerSuspend];
 }
 
 
